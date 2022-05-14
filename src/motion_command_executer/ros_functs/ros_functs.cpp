0399465ec445ae07d97c4e9b48240172166697e3
Rahulkannan S <rahulknns@gmail.com>
2022-05-14 22:07:28 +0530

    fixed errors

--- src/motion_command_executer/ros_functs/ros_functs.cpp
+++ src/motion_command_executer/ros_functs/ros_functs.cpp
@@ -1,15 +1,10 @@
-/*#include "ros_functs.h"
+#include "ros_functs.h"
 #include "ros/ros.h"
 #include "motion_commands.h"
-#include "std_msgs/Float32MultiArray.h"
-#include "std_msgs/Int8MultiArray.h"
-#include "std_msgs/Float32.h"
-#include "std_msgs/Int32.h"
-#include "materov22_pioneer/motion_control_msg.h"
+#include "materov22_pioneer/pwm_values_msg.h"
+#include "materov22_pioneer/motion_commad_msg.h"
 #include "materov22_pioneer/sensor_data_msg.h"
 
-
-#include <iostream>
 
 
 extern  float target_yaw,target_depth,current_yaw,current_pitch,current_roll,current_depth;
@@ -18,17 +13,15 @@ extern int motion_command;
 ros::Subscriber sensor_data_sub,motion_command_sub;
 ros::Publisher pwm_values_pub;
 
-materov22_pioneer::motion_control_msg motion_control_g;
-materov22_pioneer::sensor_data_msg    sensor_data_g;
+materov22_pioneer::motion_command_msg motion_command_msg;
+materov22_pioneer::sensor_data_msg    sensor_data_msg;
 
 ros:: NodeHandle* nh;
-
-
-std_msgs::Int8MultiArray pwm_values_msg;
+materov22_pioneer::pwm_values_msg pwm_values_msg;
 
 
 
-void motionCommandCallBack(const  materov22_pioneer::motion_control_msgConstPtr& motion_command_msg ){
+void motionCommandCallBack(const  materov22_pioneer::motion_command_msgConstPtr& motion_command_msg ){
 
     motion_command   = motion_command_msg->command;
     if (motion_command == TURN_TO_GIVEN_DIRECTION )
@@ -56,8 +49,7 @@ void ros_init(int argc, char** argv){
     
     motion_command_sub    = (*nh).subscribe("motion_command",1,motionCommandCallBack);
     
-    pwm_values_pub = (*nh).advertise<std_msgs::Int16MultiArray>("pwm_values",5);
-    pwm_values_msg.data.resize(6);
+    pwm_values_pub = (*nh).advertise<materov22_pioneer::pwm_values_msg>("pwm_values",5);
 
    
 
@@ -70,10 +62,10 @@ void ros_init(int argc, char** argv){
 
 void publishPWMValues(int* pwm_values){
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 4; i++)
     {
         pwm_values_msg.data[i] = pwm_values[i];
     }
     
     pwm_values_pub.publish(pwm_values_msg);
-} */
+} 
