63f849c757254529774ac31c9dbb33c89e210926
Rahulkannan S <rahulknns@gmail.com>
2022-05-15 14:44:05 +0530

    max min thrust modify

--- src/motion_command_executer/thrustered_vehicle_motion_controller/thrusters_controller/thrusters_controller.h
+++ src/motion_command_executer/thrustered_vehicle_motion_controller/thrusters_controller/thrusters_controller.h
@@ -2,8 +2,8 @@
 #define THRUSTERS_CONTROLLER_H
 #define THRUSTERS_COUNT 4
 #define ZERO_THRUST_PWM 1500
-#define MAX_THRUST_PWM  1900
-#define MIN_THRUST_PWM  1100
+#define MAX_THRUST_PWM  1800
+#define MIN_THRUST_PWM  1200
 
 void initThrusters();
 void setThrustValues(int* values);
